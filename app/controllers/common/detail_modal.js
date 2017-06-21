98f6b0e5cac989afe4c51bbacaf3919ee04be4d6
tianqinisnan <smile241isnan@163.com>
2017-06-21 11:12:40 +0800

    ADD：detail-modal

--- app/controllers/common/detail_modal.js
+++ app/controllers/common/detail_modal.js
@@ -1,9 +1,359 @@
-export default function DetailModalController($scope, $modalInstance) {
-    $scope.test = 'test';
+export default function DetailModalController($scope, $modalInstance, passedScope, common, layer, $$localStorage) {
+    
+    $scope.modalDetail = 'summary';
+    $scope.detail = {};
+    $scope.detailReal = {
+        show: false,
+        data: {}
+    };
+    $scope.changeModalDetail = changeModalDetail;
     $scope.closeModal = closeModal;
+    $scope.collapseTable = collapseTable;
+    var customerInfo = passedScope.customerInfo;
+    customerInfo.mt4_id = customerInfo.mt4_id || customerInfo.mt4_real;
+    var pagesize = $$localStorage.setPagesizeLocalStorage();
+
+    $scope.pageCurrent = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getMt4TradeList(this.currentPage, 1);
+        }
+    };
+    $scope.pageHistory = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getMt4TradeList(this.currentPage, -1);
+        }
+    };
+    $scope.pageCallback = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getCallbackList(this.currentPage);
+        }
+    };
+    $scope.pageCopyCurrent = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getCurCopyMasters(this.currentPage);
+        }
+    };
+    $scope.pageCopyHistory = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getHisCopyMasters(this.currentPage);
+        }
+    };
+    $scope.pageFollowCurrent = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getFollowCurrent(this.currentPage);
+        }
+    };
+    $scope.pageFollowHistory = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getFollowHistory(this.currentPage);
+        }
+    };
+    $scope.pageRedbag = {
+        needPrevAndNext: 1,
+        needFirstAndLast: 1,
+        needDotsLinks: 1,
+        selectPage: function () {
+            getRedbagList(this.currentPage);
+        }
+    };
+
+    getDetails();
+    getCustomerRealtimeDetails();
+    // console.log(customerInfo);
+    function changeModalDetail(item) {
+        $scope.modalDetail = item;
+
+        if (item === 'current' && !$scope.detail.current_detail) {
+            getMt4TradeList(1, 1);
+        }
+        if (item === 'history' && !$scope.detail.history_detail) {
+            getMt4TradeList(1, -1);
+        }
+        if (item === 'callback' && !$scope.detail.callback_detail) {
+            getCallbackList(1);
+        }
+        if (item === 'copy' && !$scope.detail.curCopyMasters) {
+            getCurCopyMasters(1);
+            getHisCopyMasters(1);
+        }
+        if (item === 'follow' && !$scope.detail.follow_current_detail) {
+            getFollowCurrent(1);
+            getFollowHistory(1);
+        }
+        if (item === 'redbag' && !$scope.detail.redbag_detail) {
+            getRedbagList(1);
+        }
+    }
+
+    function getDetails () {
+        common.getCustomerDetails(customerInfo.mt4_id).then(function (data) {
+            // console.log(data);
+            $scope.$emit('hideLoadingWrapper');
+            if (data.is_succ) {
+                angular.extend($scope.detail, data.data);
+            } else {
+                layer({
+                    type: 'msg',
+                    message: data.message
+                });
+            }
+        });
+    }
+    function getCustomerRealtimeDetails () {
+        common.getCustomerRealtimeDetails(customerInfo.mt4_id).then(function (data) {
+            // console.log(data);
+            $scope.detailReal.show = true;
+            if (data.is_succ) {
+                $scope.detailReal.data = data.data;
+            }
+        });
+    }
+
+    function getMt4TradeList (page, type) {
+        var offset = (page - 1) * pagesize;
+        common.getMt4TradeList(customerInfo.id, type, offset, pagesize).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                if (type === 1) {
+                    angular.extend($scope.detail, {
+                        current_detail: data.data.data
+                    });
+                    angular.extend($scope.pageCurrent, {
+                        totalPage: Math.ceil(data.data.total /
+                            pagesize),
+                        currentPage: page
+                    });
+                }
+                if (type === -1) {
+                    angular.extend($scope.detail, {
+                        history_detail: data.data.data
+                    });
+                    angular.extend($scope.pageHistory, {
+                        totalPage: Math.ceil(data.data.total /
+                            pagesize),
+                        currentPage: page
+                    });
+                }
+            }
+        });
+    }
+
+    function getCallbackList (page) {
+        var offset = (page - 1) * pagesize;
+        common.getCustomerCallback(customerInfo.user_code, offset, pagesize).then(function (data) {
+            console.log(data);
+            if (data.is_succ) {
+                angular.extend($scope.detail, {
+                    callback_detail: data.data.data
+                });
+                angular.extend($scope.pageCallback, {
+                    totalPage: Math.ceil(data.data.total /
+                        pagesize),
+                    currentPage: page
+                });
+            }
+        });
+    }
+
+    function getCurCopyMasters (page) {
+        var offset = (page - 1) * pagesize;
+        common.getFollowMasterCurrent(customerInfo.mt4_id, offset, pagesize).then(function (data) {
+            // console.info(data);
+            if (data.is_succ) {
+                angular.extend($scope.detail, {
+                    curCopyMasters: data.data.data
+                });
+                angular.extend($scope.pageCopyCurrent, {
+                    totalPage: Math.ceil(data.data.total /
+                        pagesize),
+                    currentPage: page
+                });
+
+                $scope.detail.curCopyMasters.forEach(function (item, index) {
+                    item.isParent = true;
+                });
+            }
+        });
+    }
+
+    function getHisCopyMasters(page) {
+        var offset = (page - 1) * pagesize;
+        common.getFollowMasterHistory(customerInfo.mt4_id, offset, pagesize).then(function (data) {
+            // console.info(data);
+            if (data.is_succ) {
+                angular.extend($scope.detail, {
+                    hisCopyMasters: data.data.data
+                });
+                angular.extend($scope.pageCopyHistory, {
+                    totalPage: Math.ceil(data.data.total /
+                        pagesize),
+                    currentPage: page
+                });
+
+                $scope.detail.hisCopyMasters.forEach(function (item, index) {
+                    item.isParent = true;
+                });
+            }
+        });
+    }
+
+    // 表格的展开与折叠
+    function collapseTable(copy, type, $index, dataType) {
+        // console.log(copy)
+        // 字段过滤
+        if (dataType == 'current') {
+            var api = 'getCopyAmountChangeHis';
+            var listName = 'changeHistory';
+            var dataArr = 'curCopyMasters';
+            var mt4_from = 'from_mt4';
+            var mt4_to = 'to_mt4';
+            var start_time = copy.startTimestamp;
+            var end_time = null;
+        }
+        else if (dataType == 'history') {
+            var api = 'getCopyAmountChangeHis';
+            var listName = 'changeHistory';
+            var dataArr = 'hisCopyMasters';
+            var mt4_from = 'mt4_from';
+            var mt4_to = 'mt4_to';
+            var start_time = copy.startTimestamp;
+            var end_time = copy.timestamp;
+        }
+
+        angular.extend(copy, {
+            collapsed: type == 'close' ? false : true
+        });
+
+        if (type == 'open') {
+            common.getCopyAmountChangeHis({
+                mt4From: copy[mt4_from],
+                mt4To: copy[mt4_to],
+                startDate: start_time,
+                endDate: end_time
+            }).then(function (data) {
+                // console.log(data);
+                if (data.is_succ) {
+                    // 插入一项非父级元素
+                    $scope.detail[dataArr].splice($index + 1, 0, {
+                        isParent: false
+                    });
+                    $scope.detail[dataArr][$index + 1][listName] = data.data
+                }
+            });
+        }
+        else if (type == 'close') { // 移除非父级元素
+            if ($scope.detail[dataArr][$index + 1]['isParent'] == false) {
+                $scope.detail[dataArr].splice($index + 1, 1);
+            }
+        }
+
+        // console.log('$scope.detail', $scope.detail[dataArr])
+    }
+
+    function getFollowCurrent(page) {
+        var offset = (page - 1) * pagesize;
+
+        common.getFollowCurrent(customerInfo.mt4_id, offset, pagesize).then(function (data) {
+            // console.info(data);
+            if (data.is_succ) {
+                angular.extend($scope.detail, {
+                    follow_current_detail: data.data.data
+                });
+                angular.extend($scope.pageFollowCurrent, {
+                    totalPage: Math.ceil(data.data.total /
+                        pagesize),
+                    currentPage: page
+                });
+            } else {
+                angular.extend($scope.detail, {
+                    follow_current_detail: []
+                });
+                angular.extend($scope.pageFollowCurrent, {
+                    totalPage: 1,
+                    currentPage: page
+                });
+            }
+
+        });
+    }
+
+    function getFollowHistory(page) {
+        var offset = (page - 1) * pagesize;
+
+        common.getFollowHistory(customerInfo.mt4_id, offset, pagesize).then(function (data) {
+            // console.info(data);
+            if (data.is_succ) {
+                angular.extend($scope.detail, {
+                    follow_history_detail: data.data.data
+                });
+                angular.extend($scope.pageFollowHistory, {
+                    totalPage: Math.ceil(data.data.total /
+                        pagesize),
+                    currentPage: page
+                });
+            } else {
+                angular.extend($scope.detail, {
+                    follow_history_detail: []
+                });
+                angular.extend($scope.pageFollowHistory, {
+                    totalPage: 1,
+                    currentPage: page
+                });
+            }
+
+        });
+    }
+
+    function getRedbagList(page) {
+        var offset = (page - 1) * pagesize;
+
+        common.getRedbagList(customerInfo.mt4_id, offset, pagesize).then(function (data) {
+            // console.info(data);
+            if (data.is_succ) {
+                angular.extend($scope.detail, {
+                    redbag_detail: data.data.data
+                });
+                angular.extend($scope.pageRedbag, {
+                    totalPage: Math.ceil(data.data.total /
+                        pagesize),
+                    currentPage: page
+                });
+            } else {
+                angular.extend($scope.detail, {
+                    redbag_detail: []
+                });
+                angular.extend($scope.pageRedbag, {
+                    totalPage: 1,
+                    currentPage: page
+                });
+            }
+
+        });
+    }
 
     function closeModal() {
         $modalInstance.dismiss();
     }
 }
-DetailModalController.$inject = ['$scope', '$modalInstance'];
+DetailModalController.$inject = ['$scope', '$modalInstance', 'passedScope', 'common', 'layer', '$$localStorage'];
--- app/services/common.js
+++ app/services/common.js
@@ -6,6 +6,16 @@ export default class Common {
         this.settings = {
             urls: {
                 getUserOfNum: '/api/customer/keyword',
+                getCustomerDetails: '/api/customer/details',
+                getCustomerRealtimeDetails: '/api/customer/realtimeData',
+                getMt4TradeList: '/api/mt4Trade/tlist',
+                getCustomerCallback: '/api/customer/callback',
+                getFollowMasterCurrent: '/api/copy/followMaster',
+                getFollowMasterHistory: '/api/copy/followMasterHistory',
+                getFollowCurrent: '/api/copy/follow',
+                getFollowHistory: '/api/copy/followHistory',
+                getCopyAmountChangeHis: '/api/copy/changeCopyAmount',
+                getRedbagList: '/api/customer/bonus'
             }
         }
     }
@@ -19,6 +29,157 @@ export default class Common {
     getUserOfNum(keywords) {
         return this.share.publicRequest(this.settings.urls.getUserOfNum, 'GET', {
             keywords: keywords
+        });
+    }
+    /**
+     * getCustomerDetails 获取客户详情
+     *
+     * @method getCustomerDetails
+     * @param mt4Id
+     * 
+     */
+    getCustomerDetails(mt4Id) {
+        return this.share.publicRequest(this.settings.urls.getCustomerDetails, 'GET', {
+            mt4Id: mt4Id
+        });
+    }
+    /**
+     * getCustomerRealtimeDetails 获取客户实时数据
+     *
+     * @method getCustomerRealtimeDetails
+     * @param mt4Id
+     * 
+     */
+    getCustomerRealtimeDetails(mt4Id) {
+        return this.share.publicRequest(this.settings.urls.getCustomerRealtimeDetails, 'GET', {
+            mt4Id: mt4Id
+        });
+    }
+    /**
+     * getMt4TradeList 获取客户交易历史/当前持仓
+     *
+     * @method getMt4TradeList
+     * @param customerId    id
+     * @param type          1当前持仓 -1交易历史
+     * @param offset
+     * @param limit
+     * 
+     */
+    getMt4TradeList(id, type, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getMt4TradeList, 'GET', {
+            customerId: id,
+            type: type,
+            offset: offset,
+            limit: limit
+        });
+    }
+    /**
+     * getCustomerCallback 获取客户回访记录列表
+     *
+     * @method getCustomerCallback
+     * 
+     * @param userCode      user code
+     * @param offset
+     * @param limit
+     * 
+     */
+    getCustomerCallback(userCode, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getCustomerCallback, 'GET', {
+            userCode: userCode,
+            offset: offset,
+            limit: limit
+        });
+    }
+    /**
+     * getFollowMasterCurrent 当前跟随高手列表
+     *
+     * @method getFollowMasterCurrent
+     * @param mt4Id    mt4Id
+     * @param offset
+     * @param limit
+     * 
+     */
+    getFollowMasterCurrent(mt4Id, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getFollowMasterCurrent, 'GET', {
+            mt4Id: mt4Id,
+            offset: offset,
+            limit: limit
+        });
+    }
+    /**
+     * getFollowMasterHistory 历史跟随高手列表
+     *
+     * @method getFollowMasterHistory
+     * @param mt4Id    mt4Id
+     * @param offset
+     * @param limit
+     * 
+     */
+    getFollowMasterHistory(mt4Id, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getFollowMasterHistory, 'GET', {
+            mt4Id: mt4Id,
+            offset: offset,
+            limit: limit
+        });
+    }
+    /**
+     * getFollowCurrent 当前跟随者列表
+     *
+     * @method getFollowCurrent
+     * @param mt4Id    mt4Id
+     * @param offset
+     * @param limit
+     * 
+     */
+    getFollowCurrent(mt4Id, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getFollowCurrent, 'GET', {
+            mt4Id: mt4Id,
+            offset: offset,
+            limit: limit
+        });
+    }
+    /**
+     * getFollowHistory 历史跟随者列表
+     *
+     * @method getFollowHistory
+     * @param mt4Id    mt4Id
+     * @param offset
+     * @param limit
+     * 
+     */
+    getFollowHistory(mt4Id, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getFollowHistory, 'GET', {
+            mt4Id: mt4Id,
+            offset: offset,
+            limit: limit
+        });
+    }
+    /**
+     * getCopyAmountChangeHis 跟单金额变动历史
+     *
+     * @method getCopyAmountChangeHis
+     * @param mt4From    高手mt4Id
+     * @param mt4To     跟随者mt4Id
+     * @param startDate 开始时间
+     * @param endDate   结束时间
+     */
+    getCopyAmountChangeHis(params) {
+        return this.share.publicRequest(this.settings.urls.getCopyAmountChangeHis, 'GET', params);
+    }
+    /**
+     * getRedbagList 获取用户的红包列表
+     *
+     * @method getRedbagList
+     * @param mt4Id    mt4Id
+     * @param offset
+     * @param limit
+     * 
+     */
+    getRedbagList(mt4Id, offset, limit) {
+        return this.share.publicRequest(this.settings.urls.getRedbagList, 'GET', {
+            mt4Id: mt4Id,
+            offset: offset,
+            limit: limit
         });
     }
 }

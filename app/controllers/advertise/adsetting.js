545a1d15f50b2100fded78ff5f48b4844d095371
tianqinisnan <smile241isnan@163.com>
2017-06-29 16:48:09 +0800

    ADD: 广告位设置

--- app/controllers/advertise/adsetting.js
+++ app/controllers/advertise/adsetting.js
@@ -1,4 +1,267 @@
 export default function AdvertiseAdsettingController($scope, advertise, layer) {
-    
+
+    $scope.advertiseLst = [];
+    $scope.addAdInfo = {
+        name: undefined,
+        desc: undefined,
+        identifier: undefined
+    };
+    $scope.editAdInfo = {
+        name: undefined,
+        desc: undefined,
+        identifier: undefined
+    };
+    $scope.editTagInfo = {
+        desc: undefined
+    };
+    $scope.changeTagLstSt = changeTagLstSt;
+    $scope.addSettingEle = addSettingEle;
+    $scope.updateSettingEle = updateSettingEle;
+    $scope.submitUpdateSettingEle = submitUpdateSettingEle;
+    $scope.deleteSettingEle = deleteSettingEle;
+    $scope.cancelEditInfo = cancelEditInfo;
+    $scope.addSettingTag = addSettingTag;
+    $scope.updateSettingTag = updateSettingTag;
+    $scope.submitUpdateSettingTag = submitUpdateSettingTag;
+    $scope.deleteSettingTag = deleteSettingTag;
+
+    getSettingList();
+    function getSettingList () {
+        advertise.getSettingList().then(function (data) {
+            // console.log(data);
+            $scope.advertiseLst = data.data.data;
+
+            angular.forEach($scope.advertiseLst, function (value, index) {
+                value.isOpen = false;
+                value.isEdit = false;
+                value.isParent = true;
+            });
+        });
+    }
+
+    function getSettingTagList (ad, tagList) {
+        advertise.getSettingTagList(ad.id).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                ad.tagList = data.data.data;
+                tagList.tagList = data.data.data;
+            }
+        });
+    }
+
+    function changeTagLstSt (ad, ind) {
+        // console.log(ind);
+        ind = ind + 1;
+        if (ad.isOpen) {
+            ad.isOpen = false;
+            $scope.advertiseLst.splice(ind, 1);
+        } else {
+            ad.isOpen = true;
+            var tagList = {
+                id: ad.id,
+                isParent: false
+            };
+
+            if (ad.tagList) {
+                tagList.tagList = ad.tagList;
+                $scope.advertiseLst.splice(ind, 0, tagList);
+            } else {
+                $scope.advertiseLst.splice(ind, 0, tagList);
+                getSettingTagList(ad, $scope.advertiseLst[ind]);
+            }
+        }
+    }
+
+    function addSettingEle () {
+        advertise.addSettingEle($scope.addAdInfo).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                layer({
+                    type: 'msg',
+                    message: '添加新条目成功'
+                });
+                clearAddAdInfo();
+                getSettingList();
+            } else {
+                layer({
+                    type: 'msg',
+                    message: data.message
+                });
+            }
+        });
+    }
+
+    function updateSettingEle (ad) {
+        cancelEditInfo();
+        ad.isEdit = true;
+        $scope.editAdInfo = {
+            name: ad.name,
+            desc: ad.desc,
+            identifier: ad.identifier
+        };
+    }
+    function submitUpdateSettingEle (ad) {
+        advertise.updateSettingEle({
+            entryId: ad.id,
+            name: $scope.editAdInfo.name,
+            desc: $scope.editAdInfo.desc,
+            identifier: $scope.editAdInfo.identifier
+        }).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                layer({
+                    type: 'msg',
+                    message: '修改成功'
+                });
+                getSettingList();
+            } else {
+                layer({
+                    type: 'msg',
+                    message: data.message
+                });
+            }
+        });
+    }
+
+    function deleteSettingEle (ad) {
+        layer({
+            type: 'confirm',
+            message: '确认删除吗？',
+            title: '确认信息',
+            btnArr: ['确认', '取消'],
+            func1: function () {
+                advertise.deleteSettingEle(ad.id).then(function (data) {
+                    layer({
+                        type: 'close'
+                    });
+                    // console.log(data);
+                    if (data.is_succ) {
+                        getSettingList();
+                    } else {
+                        layer({
+                            type: 'msg',
+                            message: data.message
+                        });
+                    }
+                });
+            },
+            func2: function () {
+                layer({
+                    type: 'close'
+                });
+            }
+        });
+    }
+
+    function addSettingTag (ad, ind) {
+        // console.log(ad);
+        advertise.addSettingTag(ad.id, ad.addTagInfo).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                ad.addTagInfo = undefined;
+                getSettingTagList($scope.advertiseLst[ind-1], ad);
+            } else {
+                layer({
+                    type: 'msg',
+                    message: data.message
+                });
+            }
+        });
+    }
+
+    function updateSettingTag (tag) {
+        cancelEditInfo();
+        tag.isEdit = true;
+        $scope.editTagInfo = {
+            desc: tag.desc
+        };
+    }
+    function submitUpdateSettingTag (tag, ad) {
+        advertise.updateSettingTag(tag.id, $scope.editTagInfo.desc).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                angular.forEach($scope.advertiseLst, function (value, index) {
+                    // 找到相同id的上一项，刷新列表
+                    if (value.id === ad.id) {
+                        getSettingTagList(value, ad);
+                    }
+                });
+            } else {
+                layer({
+                    type: 'msg',
+                    message: data.message
+                });
+            }
+        });
+    }
+
+    function deleteSettingTag (tag, ad) {
+        layer({
+            type: 'confirm',
+            message: '确认删除吗？',
+            title: '确认信息',
+            btnArr: ['确认', '取消'],
+            func1: function () {
+                advertise.deleteSettingTag(tag.id).then(function (data) {
+                    layer({
+                        type: 'close'
+                    });
+                    // console.log(data);
+                    if (data.is_succ) {
+                        angular.forEach($scope.advertiseLst, function (value, index) {
+                            // 找到相同id的上一项，刷新列表
+                            if (value.id === ad.id) {
+                                getSettingTagList(value, ad);
+                            }
+                        });
+                    } else {
+                        layer({
+                            type: 'msg',
+                            message: data.message
+                        });
+                    }
+                });
+            },
+            func2: function () {
+                layer({
+                    type: 'close'
+                });
+            }
+        });
+    }
+
+    function clearAddAdInfo () {
+        $scope.addAdInfo = {
+            name: undefined,
+            desc: undefined,
+            identifier: undefined
+        };
+    }
+
+    function clearEditAdInfo () {
+        $scope.editAdInfo = {
+            name: undefined,
+            desc: undefined,
+            identifier: undefined
+        };
+    }
+    function clearEditTagInfo () {
+        $scope.editTagnfo = {
+            desc: undefined
+        };
+    }
+    function cancelEditInfo () {
+        clearEditAdInfo();
+        clearEditTagInfo();
+        angular.forEach($scope.advertiseLst, function (value, index) {
+            value.isEdit = false;
+
+            if (!value.isParent && value.tagList.length) {
+                angular.forEach(value.tagList, function (value2, index2) {
+                    value2.isEdit = false;
+                });
+            }
+        });
+    }
 }
 AdvertiseAdsettingController.$inject = ['$scope', 'advertise', 'layer'];
--- app/services/advertise.js
+++ app/services/advertise.js
@@ -9,16 +9,24 @@ export default class Advertise {
                 updateAdvertise: '/api/entry/update',
                 addAdvertise: '/api/entry/add',
                 uploadFile: '/api/upload/image',
-                deleteAdvertise: '/api/entry/del'
+                deleteAdvertise: '/api/entry/del',
+                getSettingList: '/api/entry/eList',
+                getSettingTagList: '/api/entry/tList',
+                addSettingEle: '/api/entry/addE',
+                updateSettingEle: '/api/entry/updateE',
+                deleteSettingEle: '/api/entry/delE',
+                addSettingTag: '/api/entry/addT',
+                updateSettingTag: '/api/entry/updateT',
+                deleteSettingTag: '/api/entry/delT'
             }
         }
     }
     /**
      * 
      *
-     * @method getTraderList
+     * @method getAdvertiseList
      * @param {json} params 
-     *      type  广告类型 0弹窗广告 1焦点图
+     *      type  广告类型 弹窗广告, 焦点图
      */
     getAdvertiseList(type) {
         return this.share.publicRequest(this.settings.urls.advertiseList, 'GET', {
@@ -77,7 +85,7 @@ export default class Advertise {
 
     /**
      * 
-     * @method updateAdvertise  删除广告
+     * @method deleteAdvertise  删除广告
      * 
      * @param {json} params 
      *      recId        广告id
@@ -87,7 +95,111 @@ export default class Advertise {
             recId: recId
         });
     }
-    
+
+    /**
+     * 
+     *
+     * @method getSettingList 获取条目列表
+     * @param {json} params 
+     *      
+     */
+    getSettingList() {
+        return this.share.publicRequest(this.settings.urls.getSettingList, 'GET');
+    }
+
+    /**
+     * 
+     *
+     * @method getSettingTagList 获取条目内标签列表
+     * @param {json} params 
+     *      entryId     条目id
+     */
+    getSettingTagList(entryId) {
+        return this.share.publicRequest(this.settings.urls.getSettingTagList, 'GET', {
+            entryId: entryId
+        });
+    }
+    /**
+     * 
+     * @method addSettingEle  新增条目
+     * 
+     * @param {json} params 
+     *      name        条目名称
+     *      desc        条目描述
+     *      identifier  条目的区分标识
+     *      
+     */
+    addSettingEle(params) {
+        return this.share.publicRequest(this.settings.urls.addSettingEle, 'POST', params);
+    }
+    /**
+     * 
+     * @method updateSettingEle  修改条目
+     * 
+     * @param {json} params 
+     *      entryId     条目id
+     *      name        条目名称
+     *      desc        条目描述
+     *      identifier  条目的区分标识
+     *      
+     */
+    updateSettingEle(params) {
+        return this.share.publicRequest(this.settings.urls.updateSettingEle, 'POST', params);
+    }
+    /**
+     * 
+     * @method deleteSettingEle  删除广告
+     * 
+     * @param {json} params 
+     *      entryId        条目id
+     */
+    deleteSettingEle(entryId) {
+        return this.share.publicRequest(this.settings.urls.deleteSettingEle, 'DELETE', {
+            entryId: entryId
+        });
+    }
+    /**
+     * 
+     * @method addSettingTag  新增标签
+     * 
+     * @param {json} params 
+     *      entryId     条目id
+     *      desc        标签描述
+     *      
+     */
+    addSettingTag(entryId, desc) {
+        return this.share.publicRequest(this.settings.urls.addSettingTag, 'POST', {
+            entryId: entryId,
+            desc: desc
+        });
+    }
+    /**
+     * 
+     * @method updateSettingTag  修改标签
+     * 
+     * @param {json} params 
+     *      tabId       标签id
+     *      desc        标签描述
+     *      
+     */
+    updateSettingTag(tabId, desc) {
+        return this.share.publicRequest(this.settings.urls.updateSettingTag, 'POST', {
+            tabId: tabId,
+            desc: desc
+        });
+    }
+    /**
+     * 
+     * @method deleteSettingTag  删除标签
+     * 
+     * @param {json} params 
+     *      tabId        标签id
+     */
+    deleteSettingTag(tabId) {
+        return this.share.publicRequest(this.settings.urls.deleteSettingTag, 'DELETE', {
+            tabId: tabId
+        });
+    }
 }
 
 Advertise.$inject = ['share'];
